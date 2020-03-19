a6e60dd877ec68185c0967e63c7a102b218a6ae1
maxwellbenton <maxwellbenton@gmail.com>
2020-03-19 15:48:01 -0400

    set starter code messages

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def square_array(array)
+  # Use an Enumerable to square every element in the passed in array
+  # Return a new array of the results
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
+  # Return a new array of the results
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
+  # Return the boolean result of this check
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_valid_calls(planeteer_calls)
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Return the first valid call found, or return nil if no valid calls are found
 end
