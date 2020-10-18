1e03e563796e87c478adf7d07b0945e6addd9ad8
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-18 01:43:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find{|call| valid_calls.include(call)}
+  planeteer_calls.find{|call| valid_calls.include? call}
 end
